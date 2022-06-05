ed5b213ea722468a5f8d24ea11c554ecaa4ed532
SpOOk <spook.devv@gmail.com>
2022-06-05 13:03:13 -0400

    Blog Api setup

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Post
 
 # Create your tests here.
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username = "testuser",
+            email = "test@email.com",
+            password = "password1",
+        )
+        
+        cls.post = Post.objects.create(
+            author = cls.user,
+            title = "Test title",
+            body = "test body",
+        )
+        
+    # testing post model
+    def test_post_model(self):
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(self.post.title, "Test title")
+        self.assertEqual(self.post.body, "test body")
+        self.assertEqual(str(self.post), "Test title")
